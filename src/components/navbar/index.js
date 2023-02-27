46dc53af443fca60b5ada1f81c8ce53d6181c01c
Levis Sikudi <levisikudi@gmail.com>
2023-02-26 23:43:24 -0500

    navbar update

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -8,11 +8,13 @@ const Navbar = () => {
             <button className="navbar-toggler" type='button' data-bs-toggle="collapse" data-bs-target="#expandation">
                 <span className="navbar-toggler-icon" ></span>
             </button>
-            <div className="collapse navbar-collapse justify-content-evenly" id="expandation">
+            <div className="collapse navbar-collapse justify-content-end" id="expandation">
                 <ul className="navbar-nav ">
-                    <li className="nav-item mx-5"><a href="#" className="nav-link">Home</a></li>
-                    <li className="nav-item mx-5"><a href="#" className="nav-link">About</a></li>
-                    <li className="nav-item mx-5"><a href="#" className="nav-link">Contact</a></li>
+                    <li className="nav-item mx-4"><a href="#" className="nav-link">Home</a></li>
+                    <li className="nav-item mx-4"><a href="#" className="nav-link">About</a></li>
+                    <li className="nav-item mx-4"><a href="#" className="nav-link">Skills</a></li>
+                    <li className="nav-item mx-4"><a href="#" className="nav-link">Projects</a></li>
+                    <li className="nav-item mx-4"><a href="#" className="nav-link">Contact</a></li>
                 </ul> 
             </div>       
         </div>
