d7df3eb88a4b79fedbdd2de2faa5ef0df8300803
Levis Sikudi <levisikudi@gmail.com>
2023-02-27 01:59:04 -0500

    header layout

--- src/components/hero_section/index.js
+++ src/components/hero_section/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import './index.css'
+import logo from '../../images/hello-there-sticker.png'
 
 const HeroSection = () => {
   return (
-    <div className=' hero container-fluid bg-dark mb-5'>
-        <div className='container'>
-            <p className='text-success'>&lt;name&gt;</p>
-            <h1 className='text-light title'>Levi Sikudi</h1>
-            <p className='text-success'>&lt;profession&gt;</p>
-            <h2 className='display-4 text-light-emphasis'>I am a Software Engineer.</h2>
+    <div className=' hero container-fluid bg-dark py-5'>
+
+        <div className='row'>
+        
+            <div className='col d-flex row'>
+                <p className='text-success '>&lt;name&gt;</p>
+                <h1 className='text-light title '>Levi Sikudi</h1>
+                <p className='text-success '>&lt;profession&gt;</p>
+                <h2 className='display-4  text-light-emphasis'>I am a Software Engineer.</h2>
+            </div>
+        
+            <div className='col d-flex justify-content-center'>
+                <img className=''src={logo}></img>
+            </div>
+
         </div>
     </div>
   )
